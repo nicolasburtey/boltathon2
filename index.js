521dc4e196e47ff89998a7289f69b0ce6c4b8818
Nicolas Burtey <nicolas.burtey@gmail.com>
2019-12-07 09:08:17 -0800

    adding basic function for convertion a response array

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ var bignum = require('bignum');
 // rand = sha256(Math.random().toString(2))
 // r = bignum(rand, 16)
 
-init = bignum('2bdb55e8376b8857bba7f8b9aa1bab0a0c4f1212944fa584308d37f563521141', 16)
+init_hash = bignum('2bdb55e8376b8857bba7f8b9aa1bab0a0c4f1212944fa584308d37f563521141', 16)
 
-// Question templace: 0, 1, 2 or 3
-
+// Question template: 0, 1, 2 or 3, only 2 bits/answer.
 good_answers = [3, 0, 1, 3]
 
-console.log(r.toString(16))
+init_to_answer = (response, hash) => {
+    calculated_hash = hash
 
+    for (var i = 0; i < response.length; i++) {
+        // console.log(myStringArray[i]);
+        calculated_hash = calculated_hash.xor(bignum(response[i]).shiftLeft(i * 2))        
+        console.log(calculated_hash)
+    }
 
-init_to_answer = (reponse) => {
-
+    return calculated_hash
 }
 
-// r1 = init.xor(bignum('3'))
-// r2 = r1.xor(bignum('3').shiftLeft('2'))
+preimage = init_to_answer(good_answers, init_hash)
+
+console.log(init_hash.toString(16))
+console.log(preimage.toString(16))
